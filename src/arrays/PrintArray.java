604fc46a0ea945a4e3f79cdf235b48f9e757385b
SharmiSk <sharmilask313@gmail.com>
2025-05-21 16:02:27 +0530

    Added folders

--- src/arrays/PrintArray.java
+++ src/arrays/PrintArray.java
@@ -1,4 +1,24 @@
 package arrays;
 
+/*Write a program to read 5 integers from the user,
+ store them in an array, and print all the elements of the array.*/
+
+import java.util.Scanner;
+
 public class PrintArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int arr[] = new int[5];
+
+       for (int i = 0; i < 5; i++){
+           System.out.println("Enter the value at " + i+ "index: ");
+arr[i] = sc.nextInt();
+
+
+       }
+       sc.close();
+for (int i : arr){
+    System.out.println(i + " ");
+}
+    }
 }
